6c863da361636a3135da04fac53c2eb23c271280
menixator <ahmed.miljau@gmail.com>
2022-08-19 22:28:39 +0500

    Remove utils.nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,19 @@
 {
   description = "Reimplementation of the NixOS firewall with nftables";
 
-  outputs = { self, nixpkgs }:
-    let utils = import ./utils.nix;
-    in {
+  outputs = { self, nixpkgs }: {
 
-      nixosModules.nft-firewall = { config, pkgs, lib, modulesPath, ... }: {
-        imports = [ ./nft-firewall.nix ];
-      };
-
-      nixosModules.nft-nat = { config, pkgs, lib, modulesPath, ... }: {
-        imports = [ ./nft-nat.nix ];
-      };
-
-      nixosModules.default = { ... }: {
-        imports = [ self.nixosModules.nft-firewall self.nixosModules.nft-nat ];
-      };
-
+    nixosModules.nft-firewall = { config, pkgs, lib, modulesPath, ... }: {
+      imports = [ ./nft-firewall.nix ];
     };
+
+    nixosModules.nft-nat = { config, pkgs, lib, modulesPath, ... }: {
+      imports = [ ./nft-nat.nix ];
+    };
+
+    nixosModules.default = { ... }: {
+      imports = [ self.nixosModules.nft-firewall self.nixosModules.nft-nat ];
+    };
+
+  };
 }
